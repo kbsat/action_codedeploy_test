6e9d496b93f3d1036683f0b648efbacd18c0851f
BeomsooKim <kbsat@naver.com>
2021-11-26 20:06:52 +0900

    deploy.sh 수정

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/opt/testapp
 cd $REPOSITORY
 
-APP_NAME=action_codedeploy_test
+APP_NAME=testapp
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
